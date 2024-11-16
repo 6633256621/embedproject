05849334811894ef45f802102ee6cf26bcec0a24
6633256621 <6633256621@student.chula.ac.th>
2024-11-16 14:55:23 +0700

    temp and light sensor

--- embed/src/main.cpp
+++ embed/src/main.cpp
@@ -2,15 +2,29 @@
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include <FS.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 #include <SPIFFS.h>
+#include <Wire.h>
+#include <BH1750.h>
 
 const char* ssid = "Vivo 11 pro max";
 const char* password = "boom1514";
+#define DHTPIN 4    
+#define DHTTYPE DHT22
+BH1750 lightMeter;
 
 AsyncWebServer server(80);
 
+DHT dht(DHTPIN, DHTTYPE);
+
+#define I2C_SDA 33
+#define I2C_SCL 32
+
 void setup() {
     Serial.begin(115200);
+    Wire.begin(I2C_SDA, I2C_SCL);
 
     // Connect to Wi-Fi
     Serial.println("Connecting to WiFi...");
@@ -39,6 +53,12 @@ void setup() {
         file = root.openNextFile();
     }
 
+    if (!lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
+        Serial.println("Error initializing BH1750 sensor!");
+        while (1);
+    }
+    Serial.println("BH1750 sensor initialized!");
+
     // Serve index.html
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
         if (SPIFFS.exists("/index.html")) {
@@ -54,7 +74,30 @@ void setup() {
 
     // Start the server
     server.begin();
+    dht.begin();
 }
 
 void loop() {
+    delay(2000);
+    float lux = lightMeter.readLightLevel();
+    Serial.print("Light Intensity: ");
+    Serial.print(lux);
+    Serial.println(" lx");
+
+    // Read temperature and humidity
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+
+    // Check if any reading failed
+    if (isnan(humidity) || isnan(temperature)) {
+        Serial.println("Failed to read from DHT sensor!");
+        return;
+    }
+
+    // Print readings to the Serial Monitor
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print("%  Temperature: ");
+    Serial.print(temperature);
+    Serial.println("°C");
 }
